712a586efb74d34b59ccff0ec45ee7df49497eca
Josh Kraskin <Joshua.Kraskin@thetarex.com>
2021-12-27 11:50:48 -0800

    Fixed EntryTime loading bug

--- TimerApp/TimerApp/EntryTimeConverter.cs
+++ TimerApp/TimerApp/EntryTimeConverter.cs
@@ -22,7 +22,7 @@ namespace TimerApp
 
             if (string.IsNullOrEmpty(entryTime))
             {
-                return 0;
+                return value;
             }
             else
             {
